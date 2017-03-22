4c2242474855a1a72e597ac177b7f1ad8769b433
Varun Agrawal <varagrawal@gmail.com>
2017-03-22 00:42:42 -0400

    updated toc generator and toc

--- papers_list.py
+++ papers_list.py
@@ -1,8 +1,9 @@
 import os
-from os.path import isfile, join
+import os.path as osp
 import re
 
-path = 'papers/'
+
+base_path = 'papers/'
 
 def atoi(text):
     return int(text) if text.isdigit() else text
@@ -15,10 +16,31 @@ def human_sort_keys(text):
     """
     return [ atoi(c) for c in re.split('(\d+)', text) ]
 
-papers_list = [join(path, f) for f in os.listdir(path) if isfile(join(path, f))]
-papers_list.sort(key=human_sort_keys)
+def pretty_print_areas(areas):
+    for k in areas.keys():
+        print("{0}\t\t{1}".format(k, areas[k]))
 
-for p in papers_list:
-    with open(p) as f:
-        title = f.readline()[3:].strip()
-        print("- [{0}]({1})".format(title, p))
+areas = {}
+
+for root, dirs, files in os.walk(base_path):
+    # Get all the subdirectories in the base_path as the different areas
+    if root == base_path:
+        for area in dirs:
+            areas[area] = []
+
+    if files:
+        area = root.split('/')[-1]
+        for file in files:
+            areas[area].append(osp.join(root, file))
+    
+# pretty_print_areas(areas)
+
+for k in areas.keys():
+    areas[k].sort(key=human_sort_keys)
+    print("## " + k.title())
+    for p in areas[k]:
+        with open(p) as f:
+            title = f.readline()[3:].strip()
+            print("- [{0}]({1})".format(title, p))
+    
+    print()
