5ec889d5524fe650e253411c7626f979df5f0042
Varun Agrawal <varagrawal@gmail.com>
2017-10-20 19:29:51 -0400

    updated index generation script to create README files in each dir

--- papers_list.py
+++ papers_list.py
@@ -20,28 +20,45 @@ def pretty_print_areas(areas):
     for k in areas.keys():
         print("{0}\t\t{1}".format(k, areas[k]))
 
-areas = {}
+def get_papers():
+    # `dict` of various research areas. 
+    # Each key is an area string and the value is the list of papers.
+    areas = {}
 
-for root, dirs, files in os.walk(base_path):
-    # Get all the subdirectories in the base_path as the different areas
-    if root == base_path:
-        for area in dirs:
-            areas[area] = []
+    for root, dirs, files in os.walk(base_path):
+        # Get all the subdirectories in the base_path as the different areas
+        if root == base_path:
+            for area in dirs:
+                areas[area] = []
 
-    if files:
-        area = root.split('/')[-1]
-        for file in files:
-            areas[area].append(osp.join(root, file))
-    
-# pretty_print_areas(areas)
+        if files:
+            area = root.split('/')[-1]
+            for file in files:
+                if "README" in file:
+                    continue
+                areas[area].append(osp.join(root, file))
+        
+    # pretty_print_areas(areas)
+    return areas
 
-for k in areas.keys():
-    areas[k].sort(key=human_sort_keys)
-    print("## " + k.title())
-    for p in areas[k]:
-        with open(p) as f:
-            title = f.readline()[3:].strip()
-            paper_no = p.split('/')[-1][:-3]
-            print("{2}. [{0}]({1})".format(title, p, paper_no))
-    
-    print()
+
+def generate_index(area, papers):
+    papers.sort(key=human_sort_keys)
+
+    with open(osp.join(base_path, area, "README.md"), 'w+') as op:
+        op.write("## " + area.title() + "\n\n")
+        for paper in papers:
+            with open(paper) as f:
+                title = f.readline()[3:].strip()
+                paper_no = paper.split('/')[-1][:-3]
+                op.write("{2}. [{0}]({1})\n".format(title, paper, paper_no))
+
+
+def main():
+    areas = get_papers()
+    for k, v in areas.items():
+        generate_index(k, v)
+        
+
+if  __name__ == "__main__":
+    main()
